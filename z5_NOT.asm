058888db9a103ec1e3361368e278a7065fedfb85
Darkvod <piotr32123@o2.pl>
2022-09-17 16:35:56 +0200

    upload zadanek 2

--- z5_NOT.asm
+++
@@ -1,9 +0,0 @@
-# Program File: _NOT.asm 
-# Author: xxx
-# Cel podprogramu: podprogram zwraca wynik operacji NOT
-.text
-_NOT:
-move $t0,$a0   # przeniesienie wartosci z argumentow wejsciowych do rejestrow tymczasowych
-not $t1, $t0   # uzycie polecenia not - otrzymanie wyniku
-move $v1,$t1   # przeniesienie wyniku do rejestru zwracaj�cego warto��
-jr $ra         # powrot z podprogramu do programu glownego
